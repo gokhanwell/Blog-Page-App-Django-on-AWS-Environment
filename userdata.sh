b21a5397c95d1fbf0e48e4970a16d268c0ed139d
gokhanyardımcı <gokhan.yardimci@westerops.com>
2023-06-12 21:36:59 +0300

    Blog

--- userdata.sh
+++ userdata.sh
@@ -3,10 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXX"
-git clone https://$TOKEN@github.com/gokhanwell/Blog-Page-App-Django-on-AWS-Environment
+# TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXX"
+#git clone https://$TOKEN@github.com/gokhanwell/Blog-Page-App-Django-on-AWS-Environment
+git clone https://github.com/gokhanwell/Blog-Page-App-Django-on-AWS-Environment
 cd /home/ubuntu/Blog-Page-App-Django-on-AWS-Environment
 apt install python3-pip -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt-get update -y
 apt-get install python3.7-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
 cd /home/ubuntu/Blog-Page-App-Django-on-AWS-Environment/src
